342ac4f0fed383fe7f8aeadbb6a4aeac2d75e241
eli3avetta <eli3avetta.shvedchikova@gmail.com>
2022-07-17 23:03:17 +0300

    Updates

--- src/blocks/side-bar/readme.md
+++
@@ -1,7 +0,0 @@
-## Боковое меню
-
-JS:
-```bash
-  const sideBarElem = document.querySelector('#side-bar');
-  const sideBarContentElem = document.querySelector('#side-bar-content');
-```
--- src/js/common/selectInit.js
+++
@@ -1,9 +0,0 @@
-import SlimSelect from 'slim-select';
-
-export default function SelectInit(hostElem) {
-  const select = hostElem.querySelector('#slim-select');
-
-  new SlimSelect({
-    select: select
-  })
-}
