8b40287f4b0e4abcf548812b9b2fd72383839859
eli3avetta <eli3avetta.shvedchikova@gmail.com>
2022-07-17 23:40:07 +0300

    save

---
+++ src/blocks/similar-games/similar-games.js
@@ -0,0 +1,24 @@
+import Slider from "../../js/common/slider";
+
+export class SimilarGames {
+  constructor() {
+    const hostElem = document.querySelector('#similar-games-host');
+    if (!hostElem) return;
+
+    const swiperContainerElem = hostElem.querySelector('#js-swiper-container');
+    const btnPrev = hostElem.querySelector('#js-btn-prev');
+    const btnNext = hostElem.querySelector('#js-btn-next');
+
+    new Slider(
+      swiperContainerElem,
+      { mx: 5, hg: 4, md: 2, mb: 1 },
+      null,
+      btnPrev,
+      btnNext,
+      null,
+      20,
+      true);
+  }
+}
+
+new SimilarGames;
